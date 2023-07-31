b5b8fca7c5da5f18e003d690229f62ac483949eb
GregM1992 <gregmarkus1992@yahoo.com>
2023-07-30 20:35:53 -0500

    changed the radiant object to use the radiant order object

--- main.js
+++ main.js
@@ -1,123 +1,132 @@
-const radiants = [
-  {
-    id: 1,
-    name: "Kaladin Stormblessed",
-    order: "windrunner",
-    symbol: "images/Windrunner.webp",
-    spren: "honor",
-    surges: ["adhesion", "gravitation"],
-    corrupted: false
-  },
-  {
-    id: 2,
-    name: "Shallan Davar",
-    order: "lightweaver",
-    symbol: "images/Lightweaver.webp",
-    spren: "cryptic",
-    surges: ["illumination", "transformation"],
-    corrupted: false
-  },
-  {
-    id: 3,
-    name: "Lift",
-    order: "edgedancer",
-    symbol: "images/Edgedancer.webp",
-    spren: "cultivation",
-    surges: ["abrasion", "progression"],
-    corrupted: false
-  },
-  {
-    id: 4,
-    name: "Teft ",
-    order: "windrunner",
-    symbol: "images/Windrunner.webp",
-    spren: "honor",
-    surges: ["adhesion", "gravitation"],
-    corrupted: false
-  },
-  {
-    id: 5,
-    name: "Rock numuhukumakiaki'aialunamor",
-    order: "windrunner",
-    symbol: "images/Windrunner.webp",
-    spren: "honor",
-    surges: ["adhesion", "gravitation"],
-    corrupted: false
-  },
-  {
-    id: 6,
-    name: "Cephandrius Hoid",
-    order: "lightweaver",
-    symbol: "images/Lightweaver.webp",
-    spren: "cryptic",
-    surges: ["illumination", "transformation"],
-    corrupted: false
-  },
-  {
-    id:7,
-    name: "Renarin Kholin",
-    order: "truthwatcher",
-    symbol: "images/Truthwatcher.webp",
-    spren: "mist",
-    surges: ["progression","illumination"],
-    corrupted: true
-  }
-]
 const radiantOrders = [
   {
   windrunners:{
+    order: "windrunner",
     spren: "honor",
     symbol: "images/Windrunner.webp",
     surges: ["adhesion", "gravitation"]
   },
   edgedancers:{
+    order: "edgedancer",
     spren: "cultivation",
     symbol: "images/Edgedancer.webp",
     surges: ["abrasion", "progression"]
   },
   lightweavers:{
+    order: "lightweaver",
     spren: "cryptic",
     symbol: "images/Lightweaver.webp",
     surges: ["illumination", "transformation"]
   },
   elsecallers:{
+    order :"elsecaller",
     spren: "ink",
     symbol:"images/Elsecaller.webp",
     surges:["transformation","transportation"]
   },
   truthwatchers:{
+    order: "truthwatcher",
     spren: "mist",
     symbol:"images/Truthwatcher.webp",
     surges:["progression","illumination"]
   },
   willshapers:{
+    order: "willshaper",
     spren: "light",
     symbol:"images/Willshaper.webp",
     surges:["transportation","cohesion"]
   },
   stonewards:{
+    order: "stoneward",
     spren: "peak",
     symbol:"images/Stoneward.webp",
     surges:["cohesion","tension"]
   },
   bondsmiths:{
+    order: "bondsmith",
     spren: "unique",
     symbol:"images/Bondsmith.webp",
     surges:["tension","adhesion"]
   },
   skybreakers:{
+    order: "skybreaker",
     spren: "high",
     symbol:"images/Skybreaker.webp",
     surges:["gravitation","division"]
   },
   dustbringers:{
+    order: "dustbringer",
     spren: "ash",
     symbol:"images/Dustbringer.webp",
     surges:["division","abrasion"]
   }
 }
 ]
-
+const radiants = [
+  {
+    id: 1,
+    name: "Kaladin Stormblessed",
+    order: radiantOrders[0].windrunners.order,
+    symbol: radiantOrders[0].windrunners.symbol,
+    spren: radiantOrders[0].windrunners.spren,
+    surges: radiantOrders[0].windrunners.surges,
+    corrupted: false
+  },
+  {
+    id: 2,
+    name: "Shallan Davar",
+    order: radiantOrders[0].lightweavers.order,
+    symbol: radiantOrders[0].lightweavers.symbol,
+    spren: radiantOrders[0].lightweavers.spren,
+    surges: radiantOrders[0].lightweavers.surges,
+    corrupted: false
+  },
+  {
+    id: 3,
+    name: "Lift",
+    order: radiantOrders[0].edgedancers.order,
+    symbol: radiantOrders[0].edgedancers.symbol,
+    spren: radiantOrders[0].edgedancers.spren,
+    surges: radiantOrders[0].edgedancers.surges,
+    corrupted: false
+  },
+  {
+    id: 4,
+    name: "Teft ",
+    order: radiantOrders[0].windrunners.order,
+    symbol: radiantOrders[0].windrunners.symbol,
+    spren: radiantOrders[0].windrunners.spren,
+    surges: radiantOrders[0].windrunners.surges,
+    corrupted: false
+  },
+  {
+    id: 5,
+    name: "Rock numuhukumakiaki'aialunamor",
+    order: radiantOrders[0].windrunners.order,
+    symbol: radiantOrders[0].windrunners.symbol,
+    spren: radiantOrders[0].windrunners.spren,
+    surges: radiantOrders[0].windrunners.surges,
+    corrupted: false
+  },
+  {
+    id: 6,
+    name: "Cephandrius Hoid",
+    order: radiantOrders[0].lightweavers.order,
+    symbol: radiantOrders[0].lightweavers.symbol,
+    spren: radiantOrders[0].lightweavers.spren,
+    surges: radiantOrders[0].lightweavers.surges,
+    corrupted: false
+  },
+  {
+    id:7,
+    name: "Renarin Kholin",
+    order: radiantOrders[0].truthwatchers.order,
+    symbol: radiantOrders[0].truthwatchers.symbol,
+    spren: radiantOrders[0].truthwatchers.spren,
+    surges: radiantOrders[0].truthwatchers.surges,
+    corrupted: true
+  }
+]
 
 
 
